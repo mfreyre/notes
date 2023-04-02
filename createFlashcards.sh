fbfb3dd6feb006557e670946440918dbcf8340b7
marx <mfreyre@etsy.com>
2023-04-02 15:38:28 -0400

    update flashcard set

--- createFlashcards.sh
+++ createFlashcards.sh
@@ -10,5 +10,7 @@ mkdir twelve_factor_app/$directory_name
 # Create the CSV file inside the directory
 touch twelve_factor_app/$directory_name/$directory_name\_flash_cards.csv
 
+echo "Question, Answer" >> twelve_factor_app/$directory_name/$directory_name\_flash_cards.csv
+
 # Print a message to indicate successful creation of the directory and CSV file
 echo "Directory '$directory_name' and CSV file '$directory_name\_flash_cards.csv' created successfully!"
